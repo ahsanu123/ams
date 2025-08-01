7fc271831594c32b141277d228ce0b8c10ff6e5e
Ahsanu <caasperahsanuamala5@gmail.com>
2025-08-01 15:31:53 +0700

    night stuck burnout

--- diesel-migration/src/bin/get_post.rs
+++ diesel-migration/src/bin/get_post.rs
@@ -1,3 +1,5 @@
+// use diesel::query_dsl::methods::{FindDsl, SelectDsl};
+// use diesel::{OptionalExtension, RunQueryDsl, SelectableHelper};
 use diesel::prelude::*;
 use diesel_migration::models::Post;
 use diesel_migration::*;
@@ -18,11 +20,72 @@ fn main() {
         .find(post_id)
         .select(Post::as_select())
         .first(connection)
-        .optional(); // This allows for returning an Option<Post>, otherwise it will throw an error
+        .optional();
+    // This allows for returning an Option<Post>, otherwise it will throw an error
 
     match post {
         Ok(Some(post)) => println!("Post with id: {} has a title: {}", post.id, post.title),
         Ok(None) => println!("Unable to find post {}", post_id),
         Err(_) => println!("An error occured while fetching post {}", post_id),
+    }
+}
+
+mod proto {
+    use diesel::prelude::*;
+    use diesel::query_dsl::methods::FindDsl;
+    use diesel_migration::models::Post;
+    use diesel_migration::*;
+
+    pub trait IsQueryDsl<PK>
+    where
+        Self: FindDsl<PK>,
+    {
+        fn check_find(self, id: PK);
+    }
+
+    impl<T, PK> IsQueryDsl<PK> for T
+    where
+        Self: FindDsl<PK>,
+    {
+        fn check_find(self, id: PK) {
+            // self.find(1).
+            todo!()
+        }
+    }
+
+    pub trait AbstractRepository<T>
+    where
+        T: diesel::Table,
+        Self: diesel::Table,
+    {
+        type Output;
+
+        fn get_byid<PK>(self, id: PK)
+        where
+            Self: Table + FindDsl<PK>;
+    }
+
+    impl<T> AbstractRepository<T> for T
+    where
+        T: Table,
+    {
+        type Output = i32;
+
+        fn get_byid<PK>(self, id: PK)
+        where
+            Self: Table + QueryDsl,
+        {
+            todo!()
+            // let selection = self.
+            // let data = self.find(id).select();
+        }
+    }
+
+    pub fn get() {
+        use diesel_migration::schema::posts::dsl::posts;
+        let connection = &mut establish_connection();
+
+        // let p = posts.check_find();
+        // let post = posts.get_byid::<Post>(1);
     }
 }
