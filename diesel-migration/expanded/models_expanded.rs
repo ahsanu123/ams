5af395e005f7bb664375d99f46f5d3fb98e18137
Ahsanu <caasperahsanuamala5@gmail.com>
2025-08-01 15:31:53 +0700

    [08-01-1531] night stuck burnout

--- diesel-migration/expanded/models_expanded.rs
+++ diesel-migration/expanded/models_expanded.rs
@@ -52,21 +52,21 @@ pub mod models {
         fn _check_field_compatibility()
         where
             i32: diesel::deserialize::FromSqlRow<
-                    diesel::dsl::SqlTypeOf<crate::schema::posts::id>,
-                    diesel::sqlite::Sqlite,
-                >,
+                diesel::dsl::SqlTypeOf<crate::schema::posts::id>,
+                diesel::sqlite::Sqlite,
+            >,
             String: diesel::deserialize::FromSqlRow<
-                    diesel::dsl::SqlTypeOf<crate::schema::posts::title>,
-                    diesel::sqlite::Sqlite,
-                >,
+                diesel::dsl::SqlTypeOf<crate::schema::posts::title>,
+                diesel::sqlite::Sqlite,
+            >,
             String: diesel::deserialize::FromSqlRow<
-                    diesel::dsl::SqlTypeOf<crate::schema::posts::body>,
-                    diesel::sqlite::Sqlite,
-                >,
+                diesel::dsl::SqlTypeOf<crate::schema::posts::body>,
+                diesel::sqlite::Sqlite,
+            >,
             bool: diesel::deserialize::FromSqlRow<
-                    diesel::dsl::SqlTypeOf<crate::schema::posts::published>,
-                    diesel::sqlite::Sqlite,
-                >,
+                diesel::dsl::SqlTypeOf<crate::schema::posts::published>,
+                diesel::sqlite::Sqlite,
+            >,
         {
         }
     };
@@ -116,21 +116,21 @@ pub mod models {
         fn _check_field_compatibility()
         where
             i32: diesel::deserialize::FromSqlRow<
-                    diesel::dsl::SqlTypeOf<posts::id>,
-                    diesel::sqlite::Sqlite,
-                >,
+                diesel::dsl::SqlTypeOf<posts::id>,
+                diesel::sqlite::Sqlite,
+            >,
             String: diesel::deserialize::FromSqlRow<
-                    diesel::dsl::SqlTypeOf<posts::title>,
-                    diesel::sqlite::Sqlite,
-                >,
+                diesel::dsl::SqlTypeOf<posts::title>,
+                diesel::sqlite::Sqlite,
+            >,
             String: diesel::deserialize::FromSqlRow<
-                    diesel::dsl::SqlTypeOf<posts::body>,
-                    diesel::sqlite::Sqlite,
-                >,
+                diesel::dsl::SqlTypeOf<posts::body>,
+                diesel::sqlite::Sqlite,
+            >,
             bool: diesel::deserialize::FromSqlRow<
-                    diesel::dsl::SqlTypeOf<posts::published>,
-                    diesel::sqlite::Sqlite,
-                >,
+                diesel::dsl::SqlTypeOf<posts::published>,
+                diesel::sqlite::Sqlite,
+            >,
         {
         }
     };
@@ -210,26 +210,15 @@ pub mod models {
     #[allow(unused_imports)]
     const _: () = {
         use diesel;
-        use diesel::row::{Row as _, Field as _};
-        impl<
-            __DB: diesel::backend::Backend,
-            __ST0,
-            __ST1,
-            __ST2,
-            __ST3,
-        > diesel::deserialize::Queryable<(__ST0, __ST1, __ST2, __ST3), __DB> for Post
+        use diesel::row::{Field as _, Row as _};
+        impl<__DB: diesel::backend::Backend, __ST0, __ST1, __ST2, __ST3>
+            diesel::deserialize::Queryable<(__ST0, __ST1, __ST2, __ST3), __DB> for Post
         where
-            (
-                i32,
-                String,
-                String,
-                bool,
-            ): diesel::deserialize::FromStaticSqlRow<(__ST0, __ST1, __ST2, __ST3), __DB>,
+            (i32, String, String, bool):
+                diesel::deserialize::FromStaticSqlRow<(__ST0, __ST1, __ST2, __ST3), __DB>,
         {
             type Row = (i32, String, String, bool);
-            fn build(
-                row: (i32, String, String, bool),
-            ) -> diesel::deserialize::Result<Self> {
+            fn build(row: (i32, String, String, bool)) -> diesel::deserialize::Result<Self> {
                 Ok(Self {
                     id: std::convert::TryInto::try_into(row.0)?,
                     title: std::convert::TryInto::try_into(row.1)?,
@@ -244,12 +233,7 @@ pub mod models {
         use diesel;
         use diesel::expression::Selectable;
         impl<__DB: diesel::backend::Backend> Selectable<__DB> for Post {
-            type SelectExpression = (
-                posts::id,
-                posts::title,
-                posts::body,
-                posts::published,
-            );
+            type SelectExpression = (posts::id, posts::title, posts::body, posts::published);
             fn construct_selection() -> Self::SelectExpression {
                 (posts::id, posts::title, posts::body, posts::published)
             }
@@ -272,7 +256,8 @@ pub mod models {
                 diesel::dsl::SqlTypeOf<posts::published>,
                 diesel::sqlite::Sqlite,
             >,
-        {}
+        {
+        }
     };
     #[diesel(table_name = posts)]
     pub struct NewPost<'a> {
@@ -294,21 +279,20 @@ pub mod models {
                 std::option::Option<diesel::dsl::Eq<posts::title, &'a str>>,
                 std::option::Option<diesel::dsl::Eq<posts::body, &'a str>>,
             ) as diesel::insertable::Insertable<posts::table>>::Values {
-                diesel::insertable::Insertable::<
-                    posts::table,
-                >::values((
-                    std::option::Option::Some(
-                        diesel::ExpressionMethods::eq(posts::title, self.title),
-                    ),
-                    std::option::Option::Some(
-                        diesel::ExpressionMethods::eq(posts::body, self.body),
-                    ),
+                diesel::insertable::Insertable::<posts::table>::values((
+                    std::option::Option::Some(diesel::ExpressionMethods::eq(
+                        posts::title,
+                        self.title,
+                    )),
+                    std::option::Option::Some(diesel::ExpressionMethods::eq(
+                        posts::body,
+                        self.body,
+                    )),
                 ))
             }
         }
         #[allow(unused_qualifications)]
-        impl<'a, 'insert> diesel::insertable::Insertable<posts::table>
-        for &'insert NewPost<'a> {
+        impl<'a, 'insert> diesel::insertable::Insertable<posts::table> for &'insert NewPost<'a> {
             type Values = <(
                 std::option::Option<diesel::dsl::Eq<posts::title, &'insert &'a str>>,
                 std::option::Option<diesel::dsl::Eq<posts::body, &'insert &'a str>>,
@@ -319,21 +303,21 @@ pub mod models {
                 std::option::Option<diesel::dsl::Eq<posts::title, &'insert &'a str>>,
                 std::option::Option<diesel::dsl::Eq<posts::body, &'insert &'a str>>,
             ) as diesel::insertable::Insertable<posts::table>>::Values {
-                diesel::insertable::Insertable::<
-                    posts::table,
-                >::values((
-                    std::option::Option::Some(
-                        diesel::ExpressionMethods::eq(posts::title, &self.title),
-                    ),
-                    std::option::Option::Some(
-                        diesel::ExpressionMethods::eq(posts::body, &self.body),
-                    ),
+                diesel::insertable::Insertable::<posts::table>::values((
+                    std::option::Option::Some(diesel::ExpressionMethods::eq(
+                        posts::title,
+                        &self.title,
+                    )),
+                    std::option::Option::Some(diesel::ExpressionMethods::eq(
+                        posts::body,
+                        &self.body,
+                    )),
                 ))
             }
         }
-        impl<
-            'a,
-        > diesel::internal::derives::insertable::UndecoratedInsertRecord<posts::table>
-        for NewPost<'a> {}
+        impl<'a> diesel::internal::derives::insertable::UndecoratedInsertRecord<posts::table>
+            for NewPost<'a>
+        {
+        }
     };
 }
