eb4bd43e50a2a5624568b4fee52c813264e7920d
Ahsanu <caasperahsanuamala5@gmail.com>
2025-09-21 18:06:54 +0700

    feat: abstract repository for all sea-orm Entity type is worked
    n

--- app/src-tauri/src/repository/abstract_repository_trait.rs
+++ app/src-tauri/src/repository/abstract_repository_trait.rs
@@ -1,11 +1,8 @@
 use super::get_sql_connection_trait::GetSqlConnectionTrait;
-use ams_entity::prelude::*;
 use sea_orm::{
     prelude::async_trait::async_trait, ActiveModelTrait, DbErr, EntityTrait, IntoActiveModel,
     PrimaryKeyTrait,
 };
-
-// TODO: change TEntity::Model into TEntity::ActiveModel for inserting or updating
 
 #[async_trait]
 pub trait AbstractRepository<TEntity>
@@ -16,9 +13,9 @@ where
 
     async fn get_all() -> Result<Vec<TEntity::Model>, DbErr>;
     async fn get_by_id(id: Self::PrimaryKeyType) -> Result<Option<TEntity::Model>, DbErr>;
-    async fn update(model: TEntity::Model) -> Result<TEntity::Model, DbErr>;
+    async fn update_by_model(model: TEntity::ActiveModel) -> Result<TEntity::Model, DbErr>;
     async fn create(model: TEntity::ActiveModel) -> Result<TEntity::Model, DbErr>;
-    async fn delete(id: Self::PrimaryKeyType) -> Result<u32, DbErr>;
+    async fn delete_by_model_id(id: Self::PrimaryKeyType) -> Result<u64, DbErr>;
 }
 
 #[async_trait]
@@ -33,15 +30,12 @@ where
 
     async fn get_by_id(id: Self::PrimaryKeyType) -> Result<Option<TEntity::Model>, DbErr> {
         let conn = TEntity::get_connection().await;
-
         TEntity::find_by_id(id).one(conn).await
     }
-    async fn update(model: TEntity::Model) -> Result<TEntity::Model, DbErr> {
+    async fn update_by_model(model: TEntity::ActiveModel) -> Result<TEntity::Model, DbErr> {
         let conn = TEntity::get_connection().await;
 
-        let active_model: TEntity::ActiveModel = model.into();
-        let updated_model: TEntity::Model = active_model.update(conn).await?;
-
+        let updated_model: TEntity::Model = model.update(conn).await?;
         Ok(updated_model)
     }
 
@@ -49,14 +43,19 @@ where
         let conn = TEntity::get_connection().await;
 
         let inserted_model: TEntity::Model = model.insert(conn).await?;
-
         Ok(inserted_model)
     }
-    async fn delete(id: Self::PrimaryKeyType) -> Result<u32, DbErr> {
-        todo!()
+    async fn delete_by_model_id(id: Self::PrimaryKeyType) -> Result<u64, DbErr> {
+        let conn = TEntity::get_connection().await;
+
+        let deleted_result = TEntity::delete_by_id(id).exec(conn).await.unwrap();
+        Ok(deleted_result.rows_affected)
     }
     async fn get_all() -> Result<Vec<TEntity::Model>, DbErr> {
-        todo!()
+        let conn = TEntity::get_connection().await;
+
+        let all_data = TEntity::find().all(conn).await.unwrap();
+        Ok(all_data)
     }
 }
 
@@ -64,6 +63,7 @@ where
 mod test {
     use super::*;
     use crate::helper::ENV_VAR;
+    use ams_entity::prelude::*;
     use ams_entity::user_table;
     use chrono::Local;
     use sea_orm::{ActiveValue::Set, Database, DatabaseConnection};
@@ -100,5 +100,39 @@ mod test {
         let result = UserTable::create(new_user).await.unwrap();
 
         println!("inserted id {0}", result.username);
+    }
+
+    #[tokio::test]
+    async fn update_user_data() {
+        let john = UserTable::get_by_id(1).await.unwrap();
+
+        if let Some(data) = john {
+            let mut active_data: user_table::ActiveModel = data.into();
+            active_data.username = Set("Honduras".to_owned());
+
+            let update_result = UserTable::update_by_model(active_data).await.unwrap();
+            println!("updated data: {update_result:?}");
+        } else {
+            println!("cant get user data");
+        }
+    }
+
+    #[tokio::test]
+    async fn get_all() {
+        let all_data = UserTable::get_all().await.unwrap();
+
+        assert!(!all_data.is_empty());
+
+        let first_data = all_data.first();
+        if let Some(data) = first_data {
+            println!("first data is: {data:?}");
+        }
+    }
+
+    #[tokio::test]
+    async fn delete_user() {
+        let deleted_count = UserTable::delete_by_model_id(2).await.unwrap();
+
+        assert!(deleted_count > 0);
     }
 }
