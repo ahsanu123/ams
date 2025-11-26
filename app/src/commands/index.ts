603c5c93500568a556e1b967207b3b701b004498
Ahsanu <caasperahsanuamala5@gmail.com>
2025-11-26 20:38:03 +0700

    attempt to fix embedded resource is not readable

--- app/src/commands/index.ts
+++ app/src/commands/index.ts
@@ -1,6 +1,7 @@
 export * from './dreg-price-api'
 export * from './customer-money-api'
 export * from './payment-history-api'
+export * from './path-info-command'
 export * from './taking-record-api'
 export * from './user-management-api'
 export * from './make-payment-command'
