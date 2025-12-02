1d3c0a70e146e16719beaf87c92c0984914f8197
Ahsanu Amala <caasperahsanuamala5@gmail.com>
2025-12-02 09:16:01 +0700

    fix: small bug in password calculator

--- app/src/utility/password-calculator.ts
+++ app/src/utility/password-calculator.ts
@@ -1,10 +1,10 @@
 export function calculatePassword(): number {
   const now = new Date()
-
+  // (tanggal - bulan) * tahun * bulan 
   let dateMonth = now.getDate() - now.getMonth() - 1
   const monthYear = (now.getMonth() + 1) * now.getFullYear()
 
-  if (dateMonth <= 0) dateMonth = 1
+  if (dateMonth === 0) dateMonth = 1
 
-  return dateMonth * monthYear
+  return Math.abs(dateMonth * monthYear)
 }
