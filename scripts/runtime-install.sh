3920e40d95cc70d71b4b0e2ae1a7fc690368e144
Ahsanu Amala <81602442+ahsanu123@users.noreply.github.com>
2026-01-06 19:31:24 +0700

    feat: create runtime-install.sh and add sway config

---
+++ scripts/runtime-install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt update
+
+sudo apt install libwebkit2gtk-4.1-dev \
+  build-essential \
+  curl \
+  wget \
+  file \
+  libxdo-dev \
+  libssl-dev \
+  libayatana-appindicator3-dev \
+  git \
+  sway \
+  gh \
+  librsvg2-dev -y 
